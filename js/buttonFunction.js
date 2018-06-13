3a5532146ffe88a03fd306bff7f2d11080975b73
sagiGL <sagigleizer@gmail.com>
2018-06-13 15:48:25 +0300

    auto send 5 AJAX request when opening the page

---
+++ js/buttonFunction.js
@@ -0,0 +1,28 @@
+var statusDisplay = document.getElementById("d1");
+var httpRequestCount = 0;
+
+
+function sendAjaxReuquest(number) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            httpRequestCount++;
+            console.log(httpRequestCount);
+            statusDisplay.innerHTML=  "http response count: " + httpRequestCount;
+            if (httpRequestCount === 5){
+                alert("recived " + number + "http response");
+            }
+        }
+    };
+    xhttp.open("GET", "data/response.txt", true);
+    xhttp.send();
+}
+
+function sendMultiHttpRequest(number){
+    for (var i = 0; i < number; i++){
+        sendAjaxReuquest();
+    }
+    httpRequestCount = 0;
+}
+
+sendMultiHttpRequest(5);
